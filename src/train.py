ca110dda0535858a9656e1f1d55337cf4cdac55a
Henok Birru <henokbirru@aruba-eduroam-170-194.abo.fi>
2023-01-25 17:57:22 +0200

    Fix decoder last experiment with avg occupancy reward line

--- src/train.py
+++ src/train.py
@@ -61,7 +61,6 @@ def train(config):
         pbar.set_description(
             f"Agent Total reward: {agent_reward:.1%} | "
             f"Critic pred. reward: {predicted_reward:.1%}"
-            f"Avg occup. reward: {agent_avg_occ:.1%}"
         )
         
         if (i % 1000 == 0 and i > 0) or i == config.n_episodes - 1:
@@ -74,26 +73,25 @@ def train(config):
                 [
                     agent_rewards['total_reward'],
                     agent_rewards['critical_reward'],
-                    agent_rewards['avg_occupancy'],
                     [total_nf_reward] * config.n_episodes,
                     [total_ff_reward] * config.n_episodes,
                     [total_ffd_reward] * config.n_episodes,
                 ],
-                ["DRL Agent total_reward ","DRL Agent Critical Reward", "DRL Agent Avg Occupancy Reward",  "NF", "FF", "FFD"],
-                outfilepath="../experiments/policy_dnn_10_20_NF_3_Decoder.png",
-                moving_avg_window=200,
+                ["DRL Agent total_reward ","DRL Agent Critical Reward",  "NF", "FF", "FFD"],
+                outfilepath="/Users/henokbirru/Documents/EDISS/CourseMaterials/DIE/CADES/implementation/bin-packing-drl/experiments/policy_dnn_10_20_NF_3_Decoder_2L.png",
+                moving_avg_window=100,
             )
 
 
     # Save key training metrics
-    if not os.path.isfile("../experiments/experiments.csv"):
+    if not os.path.isfile("/Users/henokbirru/Documents/EDISS/CourseMaterials/DIE/CADES/implementation/bin-packing-drl/experiments/experiments.csv"):
         col_headers = list(vars(config).keys())
         col_headers.extend(["agent_reward", "nf_reward", "ff_reward", "ffd_reward"])
-        with open("../experiments/experiments.csv", "w") as f:
+        with open("/Users/henokbirru/Documents/EDISS/CourseMaterials/DIE/CADES/implementation/bin-packing-drl/experiments/experiments.csv", "w") as f:
             writer = csv.writer(f)
             writer.writerow(col_headers) 
     
-    with open("../experiments/experiments.csv", "a") as f:
+    with open("/Users/henokbirru/Documents/EDISS/CourseMaterials/DIE/CADES/implementation/bin-packing-drl/experiments/experiments.csv", "a") as f:
         writer = csv.writer(f)
         row_values = list(vars(config).values())
         row_values.extend([np.mean(agent_rewards['total_reward'][-100:]), total_nf_reward, total_ff_reward, total_ffd_reward])
