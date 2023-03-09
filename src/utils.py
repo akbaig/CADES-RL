bbae52758c93f94ceaf0965e4e3bdb31f2ad82eb
shaheryar <shaheryartariq909@gmail.com>
2023-03-09 11:19:06 +0200

    1. Added refactoring

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,23 @@
 import numpy as np
 from collections import defaultdict
-
 import mlflow
+import sys
+from typing import Any, Dict, Tuple, Union
+from stable_baselines3.common.logger import HumanOutputFormat, KVWriter, Logger
 
-def create_experiment(experiment_name, run_name, run_metrics, model, train_graph_path=None, run_params=None,
-                      reward=None):
+
+def create_experiment(
+    experiment_name,
+    run_name,
+    run_metrics,
+    model,
+    train_graph_path=None,
+    run_params=None,
+    reward=None,
+):
     mlflow.set_experiment(experiment_name)
 
     with mlflow.start_run(run_name=run_name):
-
         if not run_params == None:
             for param in run_params:
                 mlflow.log_param(param, run_params[param])
@@ -18,11 +27,32 @@ def create_experiment(experiment_name, run_name, run_metrics, model, train_graph
 
         # mlflow.log_model(model, "model")
 
-        if not train_graph_path == None:
-            mlflow.log_artifact(train_graph_path, 'training graph')
+        # if not train_graph_path == None:
+        #     mlflow.log_artifact(train_graph_path, 'training graph')
 
-    print('Run - %s is logged to Experiment - %s' % (run_name, experiment_name))
+    print("Run - %s is logged to Experiment - %s" % (run_name, experiment_name))
 
+
+class MLflowOutputFormat(KVWriter):
+    """
+    Dumps key/value pairs into MLflow's numeric format.
+    """
+
+    def write(
+        self,
+        key_values: Dict[str, Any],
+        key_excluded: Dict[str, Union[str, Tuple[str, ...]]],
+        step: int = 0,
+    ) -> None:
+        for (key, value), (_, excluded) in zip(
+            sorted(key_values.items()), sorted(key_excluded.items())
+        ):
+            if excluded is not None and "mlflow" in excluded:
+                continue
+
+            if isinstance(value, np.ScalarType):
+                if not isinstance(value, str):
+                    mlflow.log_metric(key, value, step)
 
 
 def evaluate(model, env, num_episodes=100):
@@ -38,28 +68,36 @@ def evaluate(model, env, num_episodes=100):
     termination_cause = defaultdict(int)
     is_success = 0
     occupancy_ratio = []
-    free_space=[]
+    free_space = []
 
     for i in range(num_episodes):
         episode_rewards = []
         done = False
         obs = env.reset()
-        total_bins = obs['nodes'] * env.norm_factor
+        total_bins = obs["nodes"] * env.norm_factor
         while not done:
             # _states are only useful when using LSTM policies
             action, _states = model.predict(obs)
             # here, action, rewards and dones are arrays
             # because we are using vectorized env
             obs, reward, done, info = env.step(action)
-            if info['is_success']:
+            if info["is_success"]:
                 is_success += 1
-                remaining_node_capacities = obs['nodes'] * env.norm_factor
+                remaining_node_capacities = obs["nodes"] * env.norm_factor
                 occupancy_ratio.append(
-                    round(100 - np.mean((np.array(remaining_node_capacities) / np.array(total_bins))) * 100, 2))
+                    round(
+                        100
+                        - np.mean(
+                            (np.array(remaining_node_capacities) / np.array(total_bins))
+                        )
+                        * 100,
+                        2,
+                    )
+                )
 
             if done:
-                termination_cause[info['termination_cause']] += 1
-                all_episodes_len.append(info['episode_len'])
+                termination_cause[info["termination_cause"]] += 1
+                all_episodes_len.append(info["episode_len"])
 
             episode_rewards.append(reward)
 
@@ -67,4 +105,10 @@ def evaluate(model, env, num_episodes=100):
 
     mean_episode_reward = np.mean(all_episode_rewards)
 
-    return mean_episode_reward, (sum(all_episodes_len) / num_episodes), termination_cause, is_success, occupancy_ratio
+    return (
+        mean_episode_reward,
+        (sum(all_episodes_len) / num_episodes),
+        termination_cause,
+        is_success,
+        occupancy_ratio,
+    )
