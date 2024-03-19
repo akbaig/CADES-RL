cb71181fd3970722c05d1dfb8b42c226f3728725
Ahmad Baig <ahmad.baig@abo.fi>
2024-03-20 00:59:49 +0200

    refactor slurm scripts

--- src/rungpu.sh
+++ src/rungpu.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=cdrl_test_gpu
+#SBATCH --job-name=cades_drl_job_gpu
 #SBATCH --account=project_2008863
 #SBATCH --partition=gpu
 #SBATCH --time=04:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=8000
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8g
 #SBATCH --gres=gpu:v100:1
 
-python3 -u src/train.py
+python3 -u train.py
