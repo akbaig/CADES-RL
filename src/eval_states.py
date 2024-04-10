bfaa986942b3151e9aeb9f85bbee9f3500781533
Ahmad Baig <ahmad.baig@abo.fi>
2024-04-10 22:04:25 +0300

    Make eval_states callable

--- src/eval_states.py
+++ src/eval_states.py
@@ -1,19 +1,27 @@
 from sample_generator import generate_random_graph_with_input
 
-G, graph_dict = generate_random_graph_with_input(4)
-print(graph_dict)
-print(G)
-final_allocation = [['t1'], ['t2', 't4'], ['t3']]
 
+def get_evaluation(final_allocation, num_tasks):
+    '''
+        Get final evaluation as: Total no. of edges - no. of tasks allocated in the same node
+        
+        Params:
+        - final_allocation: 2d list of the final allocation of tasks in nodes
+                            e.g. [['t1'], ['t2', 't4'], ['t3']]
+        - num_tasks: total no. of tasks
+        
+        Returns: The total bandwidth
+        - 
+    '''
 
-no_of_tasks_in_same_node = 0
-for successors in graph_dict.values():
-    if len(successors) > 2:
-        for i in range(len(successors)):
-            for j in range(i+1, len(successors)):
-                node_1, node_2 = successors[i], successors[j]
-                if G.has_edge(node_1, node_2) and [node_1, node_2] in final_allocation:
-                    no_of_tasks_in_same_node += 1
+    G, graph_dict = generate_random_graph_with_input(num_tasks)
+    no_of_tasks_in_same_node = 0
 
-print(no_of_tasks_in_same_node)
-print(G.number_of_edges())
+    for successors in graph_dict.values():
+        if len(successors) > 2:
+            for i in range(len(successors)):
+                for j in range(i+1, len(successors)):
+                    node_1, node_2 = successors[i], successors[j]
+                    if G.has_edge(node_1, node_2) and [node_1, node_2] in final_allocation:
+                        no_of_tasks_in_same_node += 1
+    return G.number_of_edges() - no_of_tasks_in_same_node
