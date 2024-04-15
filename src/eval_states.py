6b706a752f24110b631dc8a8bc340d2be1fa0947
Ahmad Baig <ahmad.baig@abo.fi>
2024-04-15 22:11:22 +0300

    comments on eval_states

--- src/eval_states.py
+++ src/eval_states.py
@@ -1,5 +1,7 @@
 from sample_generator import generate_random_graph_with_input
 
+# if given string {t1 -> {t2 -> t4,   t3}, t5 -> {t6 -> t7} } X
+# {'t1': ['t4'], 't2': ['t3', 't4', 't5'], 't3': ['t4'], 't4': ['t5']}
 
 def get_evaluation(final_allocation, num_tasks):
     '''
@@ -25,3 +27,17 @@ def get_evaluation(final_allocation, num_tasks):
                     if G.has_edge(node_1, node_2) and [node_1, node_2] in final_allocation:
                         no_of_tasks_in_same_node += 1
     return G.number_of_edges() - no_of_tasks_in_same_node
+
+
+# Todo
+
+# get number of edges in the graph
+# get active edges in internode in the graph (current implmentation)
+# get percentage of active edges/ total edges
+# generate graphs from random
+# generate graphs from user input
+# generate spaces.Box version of the graph. Makes sure it's the same as cades_env
+### Other Notes: Should allow the model to run and infer on the code by calling separate methods (for now)
+
+
+get_evaluation([['t1'], ['t2', 't4'], ['t3']], 5)
