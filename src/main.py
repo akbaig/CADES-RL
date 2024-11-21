80bf4f21408ec67b9842eabb6c7851798591e3c6
Ahmad Baig <ahmad.baig@abo.fi>
2024-11-21 10:09:22 +0200

    feat: adding model path param for inference only mode

--- src/main.py
+++ src/main.py
@@ -5,6 +5,14 @@ from utils.mlflow import MLFlowManager
 if __name__ == "__main__":
 
     env, config = initialize_environment()
-    model = PPOModel(env, config)
+    if config.train is False and config.inference is False:
+        raise ValueError("Either train or inference mode should be enabled")
+    else if config.train is False and config.inference is True:
+        if config.model_path is None or config.model_path == "":
+            raise ValueError("model_path argument should be provided for inference mode")
+        else:
+            model = PPOModel.load(config.model_path, env, config)
+    else:
+        model = PPOModel(env, config)
     mlflow_manager = MLFlowManager(model, config)
     mlflow_manager.run()
