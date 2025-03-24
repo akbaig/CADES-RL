13354779ba6866004a14686612ba9292279ea92d
Ahmad Baig <ahmad.baig@abo.fi>
2025-03-24 15:56:59 +0200

    chore: remove imports of unused libraries from init.py

--- src/env/init.py
+++ src/env/init.py
@@ -1,10 +1,6 @@
-import random
-import numpy as np
-import torch
 from utils.config import get_config
 from env.cades_env import CadesEnv
 from stable_baselines3.common.env_checker import check_env
-from stable_baselines3.common.utils import set_random_seed
 
 def initialize_environment():
     # Load configuration
